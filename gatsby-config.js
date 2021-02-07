88d079e79da7996dc7b7d65a361a9c5145a0709e
David Ryan <dpryan@stanford.edu>
2021-02-06 17:28:36 -0800

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -17,26 +17,26 @@ module.exports = {
     // Content of the About Me section
     about: `Hi! I'm studying Computer Science and Feminist, Gender and Sexuality Studies at Stanford. `,
     // Optional: List your projects, they must have `name` and `description`. `link` is optional.
-    projects: [
-      {
-        name: 'Devfolio',
-        description:
-          'A zero-config and blazing fast personal site + blog built with GatsbyJs and TailwindCSS',
-        link: 'https://github.com/RyanFitzgerald/devfolio',
-      },
-      {
-        name: 'ChromeExtensionKit',
-        description:
-          'Kit to jump-start your Chrome extension projects with a variety of battle-tested starter templates',
-        link: 'https://chromeextensionkit.com/?ref=devfolio',
-      },
-      {
-        name: 'Another Cool Project',
-        description:
-          'Lorem ipsum dolor sit amet consectetur adipisicing elit ducimus perferendis',
-        link: 'https://github.com/RyanFitzgerald/devfolio',
-      },
-    ],
+//     projects: [
+//       {
+//         name: 'Devfolio',
+//         description:
+//           'A zero-config and blazing fast personal site + blog built with GatsbyJs and TailwindCSS',
+//         link: 'https://github.com/RyanFitzgerald/devfolio',
+//       },
+//       {
+//         name: 'ChromeExtensionKit',
+//         description:
+//           'Kit to jump-start your Chrome extension projects with a variety of battle-tested starter templates',
+//         link: 'https://chromeextensionkit.com/?ref=devfolio',
+//       },
+//       {
+//         name: 'Another Cool Project',
+//         description:
+//           'Lorem ipsum dolor sit amet consectetur adipisicing elit ducimus perferendis',
+//         link: 'https://github.com/RyanFitzgerald/devfolio',
+//       },
+//     ],
     // Optional: List your experience, they must have `name` and `description`. `link` is optional.
     experience: [
       {
